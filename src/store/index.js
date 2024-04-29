f031a0115ef54059e51450dc6f8abcbb151a072d
sukyung6999 <sukyung6999@gmail.com>
2024-04-29 15:27:09 +0900

    set: 로그인 기능 테스트 전 저장소 초기화

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import SliceOneReducer from './slice/practiceOne'
-
-export default configureStore({
-    reducer: {
-        one: SliceOneReducer
-    }
-})
