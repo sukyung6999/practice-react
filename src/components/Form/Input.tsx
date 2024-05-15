22f94ae7065717109ea458f9372fae119c437a94
sukyung6999 <sukyung6999@gmail.com>
2024-05-15 22:29:18 +0900

    try: input 컴포넌트화시키기 시도했으나 보류

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,4 +1,26 @@
-function Input() {
-  return <input type="text" />
+import { InputProps } from "../Join/JoinForm";
+
+function Input({id, label, placeholder, type, minLength, maxLength, patternValue, patternMsg, required, register, errors}: InputProps) {
+  return (
+    <div>
+      <label htmlFor={id}>{label}</label>
+      <input
+        id={id}
+        placeholder={placeholder}
+        type={type}
+        {...register(id, {
+          required,
+          minLength,
+          maxLength,
+          pattern: {
+            value: patternValue,
+            message: patternMsg,
+          },
+        })}
+        aria-invalid={errors[id] ? "true" : "false"}
+      />
+      {errors.id && <span>{errors.id.message}</span>}
+    </div>
+  );
 }
 export default Input;
--- src/components/Join/JoinForm.tsx
+++ src/components/Join/JoinForm.tsx
@@ -1,10 +1,5 @@
-import { SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm, Path, UseFormRegister } from "react-hook-form";
 
-enum QuestionList {
-  option1 = "내가 가장 좋아하는 영화는?",
-  option2 = "내가 가장 좋아하는 장소는?",
-  option3 = "내가 가장 좋아하는 색깔은?",
-}
 interface FormValue {
   id: string;
   password: string;
@@ -14,6 +9,25 @@ interface FormValue {
   domain: string;
   question: QuestionList;
   answer: string;
+}
+
+// type InputProps = {
+//   id: Path<FormValue>
+//   label: string
+//   placeholder: string
+//   type: string
+//   minLength: number
+//   maxLength: number
+//   patternValue: RegExp
+//   patternMsg: string
+//   required: string
+//   register: UseFormRegister<FormValue>
+// }
+
+enum QuestionList {
+  option1 = "내가 가장 좋아하는 영화는?",
+  option2 = "내가 가장 좋아하는 장소는?",
+  option3 = "내가 가장 좋아하는 색깔은?",
 }
 function JoinForm() {
   const {
@@ -30,10 +44,10 @@ function JoinForm() {
         <label htmlFor="id">아이디</label>
         <input
           id="id"
-          placeholder="아이디를 입력해주세요. (4~12자 영문, 숫자 입력)"
+          placeholder="아이디를 필수입력입니다. (4~12자 영문, 숫자 입력)"
           type="text"
           {...register("id", {
-            required: "아이디를 입력해주세요.",
+            required: "아이디를 필수입력입니다.",
             minLength: 4,
             maxLength: 12,
             pattern: {
@@ -41,6 +55,7 @@ function JoinForm() {
               message: "4~12자 영문, 숫자 입력"
             }
           })}
+          aria-invalid={errors.id ? "true" : "false"}
         />
         {errors.id && <span>{errors.id.message}</span>}
       </div>
@@ -48,15 +63,16 @@ function JoinForm() {
         <label htmlFor="password">비밀번호</label>
         <input
           id="password"
-          placeholder="비밀번호를 입력해주세요. (8~20자 영문, 숫자, 특수문자 조합)"
+          placeholder="비밀번호를 필수입력입니다. (8~20자 영문, 숫자, 특수문자 조합)"
           type="password"
           {...register("password", {
-            required: "비밀번호를 입력해주세요.",
+            required: "비밀번호를 필수입력입니다.",
             pattern: {
               value:  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*-+])[a-zA-Z\d~!@#$%^&*-+]{8,20}$/,
               message: "8~20자 영문, 숫자, 특수문자 조합"
             }
           })}
+          aria-invalid={errors.password ? "true" : "false"}
         />
         {errors.password && <span>{errors.password.message}</span>}
       </div>
@@ -72,6 +88,7 @@ function JoinForm() {
               message: "비밀번호를 한번 더 확인해주세요."
             }
           })}
+          aria-invalid={errors.passwordConfirm ? "true" : "false"}
         />
         {errors.passwordConfirm && <span>{errors.passwordConfirm.message}</span>}
       </div>
@@ -81,8 +98,9 @@ function JoinForm() {
           id="name"
           type="text"
           {...register("name", {
-            required: "이름을 입력해주세요.",
+            required: "이름은 필수입력입니다.",
           })}
+          aria-invalid={errors.name ? "true" : "false"}
         />
         {errors.name && <span>{errors.name.message}</span>}
       </div>
@@ -92,12 +110,13 @@ function JoinForm() {
           id="email"
           type="text"
           {...register("email", {
-            required: "이메일을 확인해주세요.",
+            required: "이메일은 필수입력입니다.",
             pattern: {
               value: /^[A-Za-z0-9+-_.]+@[a-z]+\.[a-z]+$/,
               message: "이메일을 확인해주세요."
             }
           })}
+          aria-invalid={errors.email ? "true" : "false"}
         />
         {errors.email && <span>{errors.email.message}</span>}
       </div>
@@ -105,9 +124,7 @@ function JoinForm() {
         <label htmlFor="question">아이디 찾기 질문</label>
         <select
           id="questionList"
-          {...register("question", {
-            required: "아이디 찾기 질문을 확인해주세요.",
-          })}
+          {...register("question")}
         >
           <option value={QuestionList.option1}>{QuestionList.option1}</option>
           <option value={QuestionList.option2}>{QuestionList.option2}</option>
@@ -121,8 +138,9 @@ function JoinForm() {
           id="answer"
           type="text"
           {...register("answer", {
-            required: "아이디 찾기 답변을 확인해주세요.",
+            required: "아이디 찾기 답변은 필수입력입니다.",
           })}
+          aria-invalid={errors.answer ? "true" : "false"}
         />
         {errors.answer && <span>{errors.answer.message}</span>}
       </div>
@@ -131,3 +149,4 @@ function JoinForm() {
   );
 }
 export default JoinForm;
+// export type {InputProps};
