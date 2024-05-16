920b6b7001875c9cad1aff898fa7160590f9a873
sukyung6999 <sukyung6999@gmail.com>
2024-05-16 23:21:08 +0900

    add: nav 추가, 컴포넌트 분리 시도, enum 제거할 예정

--- src/types/join.ts
+++ src/types/join.ts
@@ -9,10 +9,10 @@ interface FormValue {
   answer: string;
 }
 
-type QuestionList = {
-  option1 : "내가 가장 좋아하는 영화는?",
-  option2 : "내가 가장 좋아하는 장소는?",
-  option3 : "내가 가장 좋아하는 색깔은?",
+enum QuestionList  {
+  option1 = "내가 가장 좋아하는 영화는?",
+  option2 = "내가 가장 좋아하는 장소는?",
+  option3 = "내가 가장 좋아하는 색깔은?",
 }
 
 export type {FormValue, QuestionList};
